8252ec29e3fd0329b8843306a2a9c3cdaaceb62b
mpia <mpiazaniboni@gmail.com>
2018-08-03 18:14:44 -0300

    lolo

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'rosachin_db');
+define('DB_NAME', 'rosachina_2018');
 
 /** MySQL database username */
-define('DB_USER', 'rosachin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'I00w5w5Puh');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
