6375c5a8ea1deb6bcf47601573baa05abee09ca0
mpia <mpiazaniboni@gmail.com>
2018-08-06 01:41:08 -0300

    responsive

--- wp-content/themes/rosachina/index.php
+++ wp-content/themes/rosachina/index.php
@@ -41,9 +41,6 @@
 			</div>
 
 			<header id="masthead" class="site-header" role="banner">
-				<a class="btn-menu-mobile" href="javascript:void(0);" style="display:none;">
-					<img class="icon-bar"  src="<?php echo get_template_directory_uri(); ?>/img/icon-bar.svg" alt="rosachina">
-				</a>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php
 						wp_nav_menu( array(
